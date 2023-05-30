80983e30f3bbe68ee37d10007a9b1595ca107ee9
nimej99 <aaron9909@naver.com>
2023-05-31 00:59:10 +0900

    0530

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,46 +4,32 @@ function Game() {
 
   const [conHover, setConHover] = useState(true);
   const [joyHover, setJoyHover] = useState(true);
-
-  let conImg = '/images/common/control.svg';
-  const conHov = '/images/common/control_on.svg';
-  let joyImg = '/images/common/joystick.svg';
-  const joyHov = '/images/common/joystick_on.svg';
-
-  let control = {
-    backgroundImage: `url(${process.env.PUBLIC_URL + conImg })`,
-    backgroundRepeat: 'no-repeat',
-    width: '200px',
-    height: '200px',
-    marginRight: '100px',
-    transition:'0.5s'
-  };
-
-  let control2 = {
-    backgroundRepeat: 'no-repeat',
-    width: '200px',
-    height: '200px',
-    marginRight: '100px',
-    backgroundImage: `url(${process.env.PUBLIC_URL + conHov })`,
-    transition:'0.5s'
-  };
-
-  let joyStick = {
-    backgroundImage: `url(${process.env.PUBLIC_URL + joyImg })`,
-    backgroundRepeat: 'no-repeat',
-    width: '200px',
-    height: '200px',
-    transition:'0.5s'
-  };
-
   
-  let joyStick2 = {
-    backgroundImage: `url(${process.env.PUBLIC_URL + joyHov })`,
-    backgroundRepeat: 'no-repeat',
-    width: '200px',
-    height: '200px',
-    transition:'0.5s'
-  };
+  let conImg = '/images/common/control.svg';
+    const conHov = '/images/common/control_on.svg';
+    let joyImg = '/images/common/joystick.svg';
+    const joyHov = '/images/common/joystick_on.svg';
+
+    let control = {
+      backgroundImage: `url(${process.env.PUBLIC_URL + conImg })`,
+      backgroundRepeat: 'no-repeat'
+    };
+  
+    let control2 = {
+      backgroundImage: `url(${process.env.PUBLIC_URL + conHov })`,
+      backgroundRepeat: 'no-repeat'
+    };
+  
+    let joyStick = {
+      backgroundImage: `url(${process.env.PUBLIC_URL + joyImg })`,
+      backgroundRepeat: 'no-repeat'
+    };
+  
+    
+    let joyStick2 = {
+      backgroundImage: `url(${process.env.PUBLIC_URL + joyHov })`,
+      backgroundRepeat: 'no-repeat'
+    };
 
   return (
     <>
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
     <>
       <div className='logo'>
-        <h1>
-          {/* <Link to='/'> */}
+        <Link to='/'>
+          <h1>
             <img src={process.env.PUBLIC_URL + '/images/common/logo.svg'} alt="logo" width="43.9" height='50' />
-          {/* </Link> */}
-        </h1>
+          </h1>
+        </Link>
       </div>
     </>
   );
