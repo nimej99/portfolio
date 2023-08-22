5bef4035429b4a2120b0fc98e165d8d4b06a1057
nimej99 <121537187+nimej99@users.noreply.github.com>
2023-08-22 15:20:20 +0900

    오타

--- src/components/Web.js
+++ src/components/Web.js
@@ -16,32 +16,32 @@ function Web() {
 
   const Worked = [
     {
-      thumb:'lush.png',
-      title:'RUSH Responsive Web Project',
-      term:'2022.12 ~ 2023.01',
-      desc:'HTML CSS JavaScript',
-      href:'http://jamm.dothome.co.kr/Lush/index.html'
+      thumb: 'lush.png',
+      title: 'RUSH Responsive Web Project',
+      term: '2022.12 ~ 2023.01',
+      desc: 'HTML CSS JavaScript',
+      href: 'http://jamm.dothome.co.kr/Lush/index.html'
     },
     {
-      thumb:'hana.png',
-      title:'HANA Adaptive Web Project',
-      term:'2023.02 ~ 2023.03',
-      desc:'HTML CSS JavaScript PHP',
-      href:'http://jamm.dothome.co.kr/하나금융그룹/index.html'
+      thumb: 'hana.png',
+      title: 'HANA Adaptive Web Project',
+      term: '2023.02 ~ 2023.03',
+      desc: 'HTML CSS JavaScript PHP',
+      href: 'http://jamm.dothome.co.kr/하나금융그룹/index.html'
     },
     {
-      thumb:'ofd_admin.png',
-      title:'OFD STUDIO [ADMIN] Mobile Web Project',
-      term:'2023.03 ~ 2023.04',
-      desc:'HTML CSS JavaScript PHP',
-      href:'http://jamm.dothome.co.kr/revolution/login.php'
+      thumb: 'ofd_admin.png',
+      title: 'OFD STUDIO [ADMIN] Mobile Web Project',
+      term: '2023.03 ~ 2023.04',
+      desc: 'HTML CSS JavaScript PHP',
+      href: 'http://jamm.dothome.co.kr/revolution/login.php'
     },
     {
-      thumb:'ofd_user.png',
-      title:'OFD STUDIO [USER] Mobile Web Project',
-      term:'2023.04 ~ 2024.05',
-      desc:'HTML CSS JavaScript PHP React.js',
-      href:'http://jamm.dothome.co.kr/revolution_user/index.html'
+      thumb: 'ofd_user.png',
+      title: 'OFD STUDIO [USER] Mobile Web Project',
+      term: '2023.04 ~ 2023.05',
+      desc: 'HTML CSS JavaScript PHP React.js',
+      href: 'http://jamm.dothome.co.kr/revolution_user/index.html'
     }
   ]
 
@@ -72,7 +72,7 @@ function Web() {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        {Worked.map((work) =>( 
+        {Worked.map((work) => (
           <SwiperSlide key={work.title}>
             <Works thumb={work.thumb} title={work.title} term={work.term} desc={work.desc} href={work.href} />
           </SwiperSlide>
